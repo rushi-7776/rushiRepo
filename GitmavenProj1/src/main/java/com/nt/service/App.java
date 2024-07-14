e377101bcbe821dd4737734a4959a0e97f1ae19d
rushi <rushikeshgovekar3@gmail.com>
2024-07-14 23:09:12 +0530

    project done commit

--- GitmavenProj1/src/main/java/com/nt/service/App.java
+++ GitmavenProj1/src/main/java/com/nt/service/App.java
@@ -1,9 +1,11 @@
 package com.nt.service;
+
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
         System.out.println("Rushi");
+        System.out.println("hi i am dev");
     }
 }
